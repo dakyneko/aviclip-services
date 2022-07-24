4868133faaecbc76b15aa406463444ac89a19d35
Daky <daky+github@vneko.ch>
2022-07-24 23:34:05 +0200

    draft sqlite db

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ from pprint import pprint
 from copy import deepcopy
 from random import Random
 from types import GeneratorType as generator
+import sqlite3
 
 from fastai.vision.all import *
 from fastcore.basics import AttrDict
@@ -463,12 +464,15 @@ def plot_metrics(self: Recorder, nrows=None, ncols=None, figsize=None, **kwargs)
         ax.legend(loc='best')
     plt.show()
 
+def merge_dicts(dicts):
+    return reduce(lambda acc, x: {**acc, **x}, dicts, {})
+
 def merge_anns(anns1, anns2):
     collisions = set( anns1.keys() ).intersection( anns2.keys() ).difference([ 'name', 'tags' ])
     assert len(collisions) == 0, f'collision anns: {collisions} between {anns1} and {anns2}'
     # merge tags lists (deduplicate)
     tags = list(set( anns1.get('tags', []) + anns2.get('tags', []) ))
-    return Bunch(anns1 | anns2 | ({'tags': tags} if len(tags) > 0 else {}))
+    return Bunch(merge_dicts([anns1, anns2, ({'tags': tags} if len(tags) > 0 else {})]))
 
 def dict_nonull(**kwargs):
     return { k:v for k,v in kwargs.items() if v != None }
@@ -480,3 +484,93 @@ def jsonl_dump(xs, fd):
 def jsonl_load(fd):
     for l in fd:
         yield json.loads(l)
+
+class SQLiteDB(object):
+
+    def __init__(self, path, name, col_types):
+        '''assumes first column in col_types is the unique id'''
+        self.path = path
+        self.name = name
+
+        self.execute('pragma journal_mode = WAL')
+        self.execute('pragma mmap_size = 30000000000')
+
+        self._create_table(col_types)
+        self.cols = list(zip(*col_types))[0]
+
+        self.insert_query = 'INSERT OR IGNORE INTO %s VALUES (%s)' % (self.name, ', '.join(['?'] * len(self.cols)))
+        self.update_query = 'UPDATE %s SET %s WHERE %s = ?' % (self.name, self.unique_col, ', '.join(['%s = ?' % col for col in self.cols[1:]]))
+
+        self._create_index()
+
+    def _create_index(self):
+        self.execute('CREATE INDEX IF NOT EXISTS %s_index on %s (%s)' % (self.name, self.name, self.unique_col))
+
+    def _create_table(self, col_types):
+        self.execute('CREATE TABLE IF NOT EXISTS %s (%s)' % (self.name, ', '.join(['%s %s %s' % (c, t, 'UNIQUE' if i == 0 else '') for i, (c, t) in enumerate(col_types)])))
+
+    @property
+    def unique_col(self):
+        return self.cols[0]
+
+    def _execute(self, *args, chunk_size=1000):
+        with sqlite3.connect(self.path, timeout=1000) as con:
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+            if type(args[-1]) == types.GeneratorType:
+                cur.executemany(*args)
+            else:
+                cur.execute(*args)
+            con.commit()
+            while True:
+                results = cur.fetchmany(chunk_size)
+                if len(results) == 0:
+                    break
+                for result in results:
+                    yield result
+
+    def execute(self, *args, generator=False, **kwargs):
+        results = self._execute(*args, **kwargs)
+        if not generator:
+            results = list(results)
+        return results
+
+    def safely_execute(self, *args):
+        try:
+            self.execute(*args)
+            return True
+        except sqlite3.IntegrityError:
+            return False
+
+    def get(self, rowid):
+        results = self.execute('SELECT * FROM %s WHERE rowid = ?' % self.name, (rowid,))
+        return Bunch(results[0])
+
+    def search(self, text, col):
+        results = self.execute('SELECT * from %s WHERE %s MATCH ?' % (self.name, col), (text,))
+        for result in results:
+            yield(Bunch(result))
+
+    def size(self):
+        results = self.execute('SELECT COUNT(*) FROM %s' % self.name)
+        return results[0][0]
+
+    def iter_rows(self):
+        rows = self.execute('SELECT * FROM %s' % self.name, generator=True)
+        for row in rows:
+            yield(Bunch(row))
+
+    def contains(self, elt):
+        results = self.execute('SELECT COUNT(*) FROM %s WHERE %s = ?' % (self.name, self.unique_col), (elt[self.unique_col],))
+        count = results[0][0]
+        return count == 1
+
+    def _iter_insertable_elts(self, elts):
+        for elt in elts:
+            yield(tuple([elt.get(col, None) for col in self.cols]))
+
+    def insert(self, elts):
+        return self.safely_execute(self.insert_query, self._iter_insertable_elts(elts))
+
+    def update(self, elt):
+        return self.safely_execute(self.update_query, tuple([elt.get(col, None) for col in self.cols[1:]] + [elt[self.unique_col]]))
